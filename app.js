2431bb0259e0d117bbc1761e2f5e6ec6bcbca5a2
Juan De La Torre <juandelatorre@MacBook-Air.local>
2021-12-01 21:37:53 -0800

    js for building table with a filter

--- app.js
+++ app.js
@@ -15,3 +15,20 @@ function buildTable(data) {
         });
     });
 }
+
+// making the click function
+function handleClick() {
+    // telling D3 to look for the #datetime id in the HTML tags. chaining it with 
+    //.property("value") tells D3 to also grab that information and hold it in the 'date' variable
+    let date = d3.select("#datetime").property("value"); // grab the datetime value from the filter
+    let filteredData = tableData;
+    if (date) { // check to see if a date was entered and filter the data using that date
+        filteredData = filteredData.filter(row => row.datetime === date);
+    }; // show only the rows where the date is equal to the date filter we created above
+    buildTable(filteredData); // rebuild the table using the filtered data, if not date was entered, then it will just be the original tableData
+};
+
+d3.selectAll("#filter-btn").on("click", handleClick); //#filter-btn is the selector string
+// by adding .on('click', handleClick); we're telling D3 to execute our handleClick() function when the button with an id of filter-btn is clicked
+
+buildTable(tableData);
